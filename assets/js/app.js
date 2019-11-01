07b39bf768f45f19b6f9ddcce86d13f01ccf4242
Kieran Barker <kieran11397@gmail.com>
2019-11-01 07:13:26 +0000

    Remove unnecessary part from JSDoc

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@
    * Sanitize and encode all HTML in a user-submitted string
    * (c) 2018 Chris Ferdinandi, MIT License, https://gomakethings.com
    * @param  {String} str  The user-submitted string
-   * @return {String} str  The sanitized string
+   * @return {String}      The sanitized string
    */
   function sanitizeHTML(str) {
     var temp = document.createElement("div");
