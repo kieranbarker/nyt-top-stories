3744b309c50050239fe9d2769a9e289c2c254f1e
Kieran Barker <kieran11397@gmail.com>
2019-11-02 13:28:49 +0000

    Remove unnecessary promise

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,10 @@ var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
  
 function minify() {
-  gulp.src("./js/app.js")
+  return gulp.src("./js/app.js")
     .pipe(rename("app.min.js"))
     .pipe(uglify())
     .pipe(gulp.dest("./js"));
-  return Promise.resolve("minified");
 }
 
 exports.default = minify;
